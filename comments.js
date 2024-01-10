8c95c327e45a307859cc940c93f3657074f08312
arxify <69035413+devarxify@users.noreply.github.com>
2024-01-10 19:56:36 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,3 +1,5 @@
+// Create web server
+
 const express = require('');
 
 const app = express();
@@ -8,16 +10,12 @@ const PORT = 3000;
 // Handle HTTP route GET requests to the root URL
 
 app.get('/', (req, res) => {
-
   res.send('Hello, world!');
-
 });
 
 
 // Start the server
 
 app.listen(PORT, () => {
-
   console.log(`Server is listening on port ${PORT}`);
-
 });
